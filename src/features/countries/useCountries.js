87ec170d4def1d27cdf3cccadf7edbfaa7bbaa30
ZaforAbdullah <33929609+ZaforAbdullah@users.noreply.github.com>
2025-06-29 00:00:52 +0800

    fix: correct import path for fetchCountries in useCountries.js

--- src/features/countries/useCountries.js
+++ src/features/countries/useCountries.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchCountries } from './Api.js';
+import { fetchCountries } from './api.js';
 
 export const useCountries = () => {
     return useQuery({
