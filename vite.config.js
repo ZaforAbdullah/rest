2f4d1b984b78d155c20af4b19106926369db8d23
ZaforAbdullah <33929609+ZaforAbdullah@users.noreply.github.com>
2025-08-02 00:17:04 +0800

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
